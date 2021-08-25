ff49d6206620ab2eb40f6d1ddace0cd67632bcad
woogyung <37412073+woogyungcho@users.noreply.github.com>
2021-08-25 21:55:19 +0900

    mbti 통합보기, 지역 좋아요 결과보기_0825
    테스트 db로 mbti 통합보기, 지역 좋아요 결과보기 합산 테이터가 보여지게 셋팅

---
+++ totalmbti_0825/localdata.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbmbtitrip
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('like_local.html')
+
+## API 역할을 하는 부분
+@app.route('/likedata', methods=['POST'])
+def make_like():
+    sample_data = request.form['sample_data']
+
+    db.users.update_one({'name': sample_data}, {'$set': {'like': new_like}})
+
+    return jsonify({'msg': '좋아요 클릭완료'})
+
+
+@app.route('/likedata', methods=['GET'])
+def show_like():
+    like_data = list(db.users.find({}, {'_id': False}).sort('like',-1))
+
+    return jsonify({'like_datas': like_data})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
